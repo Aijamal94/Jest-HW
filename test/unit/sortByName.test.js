c56e948662f84897904c37188b28c9a8c7b618fc
Aizhamal <aijashka79@gmail.com>
2024-11-29 14:11:05 +0600

    Initial commit

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,26 @@
+const sorting = import("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+});
+
+describe("Books names test suit", () => {
+  it("The function should not be sorted unless it is necessary", () => {
+    const input = ["Гарри Поттер"];
+    const expected = ["Гарри Поттер"];
+    const result = sorting.sortByName(input);
+    expect(result).toEqual(["Гарри Поттер"]);
+  });
+});
